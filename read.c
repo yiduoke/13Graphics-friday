fb870bb3a96e778fb5299ed6f1e34f502693879a
Penn Wu <penn.z.wu@gmail.com>
2018-06-13 12:26:06 -0400

    passing files nothings happening

--- read.c
+++ read.c
@@ -1,37 +1,49 @@
-#include <string.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+#include "parser.h"
+#include "symtab.h"
+
+#include "matrix.h"
+#include "ml6.h"
+#include "display.h"
+#include "draw.h"
+#include "stack.h"
+#include "gmath.h"
 #include "draw.h"
 
-struct matrix *polygons parse_mesh(char* file){
-	int num_columns = 3000000;
-	struct matrix* polygons = new_matrix(4, num_columns);
+struct matrix *parse_mesh(char *file) {
+	int num_columns = 999999;
+	struct matrix *polygons = new_matrix(4, num_columns);
 	FILE *fp;
 	char str[256];
-	double meow[num_columns];// for vertices
-	int meower[4];// for faces
+	double *meow; // for vertices
+	int meower[4]; // for faces
 	char command[10];
-	int num_vertex = 0;
+	int num_vertices = 0;
 	
 	fp = fopen(file, "r");
 	if (!fp){
 		perror("cannot open this mesh file");
-		return NULL;
+		exit(0);
 	}
 	
-	while (fgets(str, sizeof(str), fp)){
-//		printf("%s", str);
-		if (!strncmp(str, "v", 1)){ // vertex command
-			printf("vertex: ");
-			
-			if (num_vertex > num_columns){
-				meow = grow_array(meow, num_columns, num_columns * 2);
-				num_columns *= 2;
-			}
-			sscanf(str, "%s %lf %lf %lf", command, meow+num_vertex, meow+num_vertex+1, meow+num_vertex+2);
-			printf("%lf, %lf, %lf\n", meow[num_vertex], meow[num_vertex+1], meow[num_vertex+2]);
-			num_vertex += 3;
+	meow = (double *)calloc(num_columns, sizeof(double));
+
+	while (fgets(str, sizeof(str), fp)) {
+		//printf("%s", str);
+		if (num_vertices > num_columns) {
+			meow = grow_array(meow, num_columns, 2 * num_columns);
+			num_columns *= 2;
 		}
-		else if (!strncmp(str, "f", 1)){ // vertex command
+		if (!strncmp(str, "v", 1)) { // vertex command
+			printf("vertex: ");
+			sscanf(str, "%s %lf %lf %lf", command, meow+num_vertices, meow+num_vertices+1, meow+num_vertices+2);
+			printf("%lf, %lf, %lf\n", meow[num_vertices], meow[num_vertices+1], meow[num_vertices+2]);
+			num_vertices += 3;
+		}
+		else if (!strncmp(str, "f", 1)) { // vertex command
 			printf("face made from vertices: ");
 			sscanf(str, "%s %d %d %d %d", command, meower, meower+1, meower+2, meower+3);
 			printf("%d, %d, %d, %d\n", meower[0], meower[1], meower[2], meower[3]);
@@ -45,10 +57,10 @@ struct matrix *polygons parse_mesh(char* file){
 														polygons->m[0][meower[2]], polygons->m[1][meower[2]], polygons->m[2][meower[2]]);
 		}
 	}
-	
+
 	fclose(fp);
-	
-	return null
+
+	return polygons;
 }
 
 int main(){
